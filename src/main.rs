cb05dcabfca64794bf57637f98912f7349fcfcf4
rooot <hey@rooot.gay>
2023-06-02 15:46:59 +0200

    feat: move player and stuff
    Signed-off-by: rooot <hey@rooot.gay>

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,9 @@ pub mod gdmp {
     include!(concat!(env!("OUT_DIR"), "/gdmp.rs"));
 }
 
+use crate::gdmp::*;
+use crate::gdmp::packet::Packet::{PlayerJoin, PlayerMove};
+
 fn main() -> anyhow::Result<()> {
     let enet = Enet::new().context("could not initialize ENet")?;
 
@@ -33,30 +36,55 @@ fn main() -> anyhow::Result<()> {
         let evnt = host
             .service(Duration::from_micros(1000))
             .context("service failed")?;
-        if evnt.is_none() {
-            continue;
-        }
-        //println!("received event: {:#?}", evnt);
 
-        let evnt = evnt.unwrap();
+        let evnt = match evnt {
+            Some(evnt) => evnt,
+            None => continue,
+        };
+
         match evnt.kind() {
             EventKind::Connect => println!("new connection!"),
             EventKind::Disconnect { .. } => println!("disconnect!"),
             EventKind::Receive {
                 channel_id,
-                ref packet
+                ref packet,
             } => {
                 let data = packet.data();
                 println!("got packet on channel {}, size {}", channel_id, data.len());
 
-                let packet:Result<gdmp::Packet, DecodeError> = prost::Message::decode(data);
-                if packet.is_err() {
-                    println!("error decoding packet: {:?}", packet);
-                    println!("data: {:?}", data);
-                    continue;
+                let packet: Result<gdmp::Packet, DecodeError> = prost::Message::decode(data);
+                let packet = match packet {
+                    Ok(packet) => packet,
+                    Err(err) => {
+                        eprintln!("error decoding packet: {}", err);
+                        eprintln!("data: {:?}", data);
+                        continue;
+                    }
+                };
+
+                let packet = match packet.packet {
+                    None => {
+                        eprintln!("invalid (empty) packet");
+                        continue;
+                    }
+                    Some(packet) => packet,
+                };
+
+                match packet {
+                    PlayerJoin(PlayerJoinPacket {
+                        room,
+                        visual: _visual,
+                    }) => {
+                        println!("player join packet - joined room {:?}", room.unwrap());
+                    }
+                    PlayerMove(PlayerMovePacket {pos_p1, pos_p2}) => {
+                        println!("player move packet - position {:?}, velocity {:?}", pos_p1.unwrap(), pos_p2.unwrap_or(Position::default()));
+                    }
+                    _ => {
+                        println!("UNIMPLEMENTED PACKET: {:#?}", packet);
+                    }
                 }
 
-                println!("packet: {:#?}", packet);
                 // save to file
                 let mut file = File::create("test2")?;
                 file.write_all(data)?;
